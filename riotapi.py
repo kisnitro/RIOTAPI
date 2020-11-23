707730087af7a89147222b1454559f2337ea53b7
kisnitro <54239388+kisnitro@users.noreply.github.com>
2020-11-23 10:24:39 +0100

    Add files via upload

--- riotapi.py
+++ riotapi.py
@@ -7,21 +7,29 @@ ff = open("apikey.txt", 'r+')
 line = ff.readline()
 api_key = str(line)
 ff.close()
+#region GET REGION and SUMMONER NAME
+global region
+sel_region = int(input("Please select region: [1]: EUNE\t[2]: EUW"))
+if(sel_region == 1):
+    region = "eun1"
+elif(sel_region == 2):
+    region = "euw1"
+
+global name
+gomb = int(input("Please choose from the following options \n [1]: Summoner name: SdnamrA \n [2]: Custom summoner name\n"))
+if (gomb == 1):
+    name = "SdnamrA"
+    print("Option [1] selected")
+elif (gomb == 2):
+    print("Option [2] selected")
+    name = input("Please enter your summoner name: ")
+#endregion
 def matchlist():
-    base_link = "https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key={api_key}"
-    match_list = "https://eun1.api.riotgames.com/lol/match/v4/matchlists/by-account/{accountId}?api_key={api_key}"
-
-    file1 = open("BARD.txt", "a")
+    base_link = "https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key={api_key}"
+    match_list = "https://{region}.api.riotgames.com/lol/match/v4/matchlists/by-account/{accountId}?api_key={api_key}"
+    file1 = open("champion.txt", "a")
     file1.close()
-    os.remove("BARD.txt")
-    gomb = int(input("Please choose from the following options \n [1]: Summoner name: SdnamrA \n [2]: Custom summoner name\n"))
-    if (gomb == 1):
-        name = "SdnamrA"
-        print("Option [1] selected")
-    elif (gomb == 2):
-        print("Option [2] selected")
-        name = input("Please enter your summoner name: ")
-
+    os.remove("champion.txt")
     gomb2 = int(input("Please choose from the following options \n [1]: Normal Draft \n [2]: SoloQ\n [3]: Flex\n"))
     if (gomb2 == 1):
         queueID = 400
@@ -33,7 +41,7 @@ def matchlist():
         queueID = 440
         print("Option [3] selected")
 
-    gomb3 = int(input("Please choose from the following options \n [1]: Custom champion ID\n [2]: Bard\n"))
+    gomb3 = int(input("Please choose from the following options \n [1]: Custom champion ID\n [2]: champion\n"))
     if (gomb3 == 1):
         print("Option [1] selected")
         champID = int(input("Please enter champion ID: "))
@@ -43,40 +51,47 @@ def matchlist():
       
 
     #   GET accountId
-    full_link = base_link.format(name=name,api_key=api_key)
+    full_link = base_link.format(region=region, name=name,api_key=api_key)
     request = requests.get(full_link)
     r = request.json()
     accountId = r["accountId"]
     print ("Your account id is: ",accountId)
     print("Getting your match list ...")
     #   GET match history
-    full_link = match_list.format(accountId=accountId,api_key=api_key)
+    full_link = match_list.format(region=region, accountId=accountId,api_key=api_key)
     request = requests.get(full_link)
     r = request.json()
-    #   show BARD matches only
+    #   show champion matches only
     for x in r['matches']:
         if (int(x['queue']) == queueID):
             if(int(x['champion']) == champID):
                 gameId = x['gameId']
                 gameId_text = str(gameId) + "\n"
-                text_file = open("BARD.txt", "a")
+                text_file = open("champion.txt", "a")
                 text_file.write(gameId_text)
                 text_file.close()
 
 def matchdetails():
-    baseLink = "https://eun1.api.riotgames.com/lol/match/v4/matches/{matchId}?api_key={api_key}"
+    baseLink = "https://{region}.api.riotgames.com/lol/match/v4/matches/{matchId}?api_key={api_key}"
     file5 = open("stats.csv", "a")
     file5.close()
     os.remove("stats.csv")
     d = ","
-    temp = open("BARD.txt",'r').read().split('\n')
+    temp = open("champion.txt",'r').read().split('\n')
     #Last 7 match
-    first = int(input("How many matches you want to be analyzed?(last match is first) \nFrom: "))
+    del temp[-1]
+    first = 0
     last = int(input("To: "))
-    last = last + 1
+    if(last >= len(temp)):
+        last = len(temp)
+    else:
+        last = last + 1
+
+    print(last)
+    print(temp)
     for x in range(first, last):
         matchId = temp[x]
-        newLink = baseLink.format(matchId=matchId, api_key=api_key)
+        newLink = baseLink.format(region=region, matchId=matchId, api_key=api_key)
         request = requests.get(newLink)
         r = request.json()
         lista = r['participantIdentities']
@@ -84,7 +99,7 @@ def matchdetails():
             player = z['player']
             partId = z['participantId']
             summoner = player['summonerName'], partId
-            if (summoner == ('SdnamrA', partId)):
+            if (summoner == ('SdnamrA', partId) or summoner == (name, partId)):
                 myid = summoner
         lista2 = r['participants']
         for y in lista2:
